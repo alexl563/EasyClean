03da22cc7804b3d70c9e3b73815b6bd8414e477f
alex lo <alo188@comcast.net>
2021-08-03 16:12:28 -0400

    code cleanup

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 var config = {
-    MY_KEY : 'pk.eyJ1IjoiYWxleHRoZXN0YXIiLCJhIjoiY2tjdGFsN3N4MXcyMTJxbGJ0aWllMDllNiJ9.dNdpbHLG9z31ZRxIaaejbw'
-  }
+  MY_KEY:
+    "pk.eyJ1IjoiYWxleHRoZXN0YXIiLCJhIjoiY2tjdGFsN3N4MXcyMTJxbGJ0aWllMDllNiJ9.dNdpbHLG9z31ZRxIaaejbw",
+};
+
+
